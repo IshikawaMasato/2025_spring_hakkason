1ba66870fddbac273f0d773046983960ab40d479
Masato <113155801+IshikawaMasato@users.noreply.github.com>
2025-05-28 23:24:15 +0900

    config.pyの作成

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # .envファイルを読み込む
+
+class Config:
+        SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+        SQLALCHEMY_TRACK_MODIFICATIONS = False
+        SECRET_KEY = os.environ.get('SECRET_KEY')
